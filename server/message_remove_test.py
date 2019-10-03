089a96fe62f38023a16eeea0483cdd5addf39ce5
Steven Lay <steven.lay@student.unsw.edu.au>
2019-10-03 22:08:51 +1000

    Added more test and formatted

--- server/message_remove_test.py
+++ server/message_remove_test.py
@@ -1,44 +1,92 @@
 import pytest
 
 def message_remove(token, message_id):
-	if message_id == null
-		raise ValueError("Message no longer exists") 
 	pass
 
 	#possible errors:
 	#value error
 	#1. message at id doesn't exist
 	#access error
-	#2. message wasn't sent by the user using message_remove
-	#3. message not sent by an owner of the channel (admin i guess? i'm not sure)
-	#4. message wasn't sent by an admin or owner of the slack
+	#2. message wasn't sent by the user using message_remove (removing someone elses message)
+	#3. message not sent by an owner of the channel (error when removing a message not sent by the owner)
+	#4. message wasn't sent by an admin or owner of the slack (error when removing a message not sent by admin)
+	
+	#Only admin can remove messages (his own messages)
 
+	#All test assume that nothing (users/channels/reacts/messages) exist prior to testing
+	#All test assume that user1 is a normal user and admin1 and admin2 are admins and owner1 is an owner of the channel
 def test_message_remove_1():
-	#To test message_send, we need user token from auth register
-	#auth_register(email,password,name_first,name_last) returns dict containing u_id, token
-	#and a channel_id from channels_create
-	#channels_create(token,name,is_public) returns dict containing id,name
 	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	ownerDict1 = auth_register('ownersteven@gmail.com','ownerhello123','ownerSteven','Lay')
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
 	channelDict1 = channels_create(user1, 'chat1',True)
 	user1 = userDict1['token']
+	owner1 = ownerDict1['token']
+	admin1 = adminDict1['token']
 	channel1 = channelDict1['channel_id']
-
-	#assuming no messages have been sent yet
-	with pytest.raises(ValueError):
-		message_remove(user1, 1)	#not legal
+	message_send(admin1, channel1, 'testing 123')	
+	message_remove(admin1, 1)
 
 def test_message_remove_2():
-	#To test message_send, we need user token from auth register
-	#auth_register(email,password,name_first,name_last) returns dict containing u_id, token
-	#and a channel_id from channels_create
-	#channels_create(token,name,is_public) returns dict containing id,name
 	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	ownerDict1 = auth_register('ownersteven@gmail.com','ownerhello123','ownerSteven','Lay')
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
 	channelDict1 = channels_create(user1, 'chat1',True)
 	user1 = userDict1['token']
+	owner1 = ownerDict1['token']
+	admin1 = adminDict1['token']
 	channel1 = channelDict1['channel_id']
-	message_send(user1,channel1, "hello")
-	message_remove(user1, 1)	#removes message id 1 assuming "hello" is contained in message_id 1		
-	with pytest.raises(ValueError):
-		message_remove(user1, 1)	#not legal (no more message in message_id 1)
+	message_send(admin1, channel1, 'test message one')	
+	message_send(admin1, channel1, 'test message two')	
+	message_remove(admin1, 1)
+	message_remove(admin1, 2)
 
 def test_message_remove_3():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	ownerDict1 = auth_register('ownersteven@gmail.com','ownerhello123','ownerSteven','Lay')
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	user1 = userDict1['token']
+	owner1 = ownerDict1['token']
+	admin1 = adminDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(admin1, channel1, 'sorry guys only admins can remove messages')	
+	message_send(user1, channel1, 'are you joking? let me test that')
+	with pytest.raises(AccessError):
+		message_remove(user1, 2)
+	message_remove(admin1, 1)
+
+def test_message_remove_4():
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	adminDict2 = auth_register('adminsteven2@gmail.com','admin2hello123','admin2Steven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	admin1 = adminDict1['token']
+	admin2 = adminDict2['token']
+	channel1 = channelDict1['channel_id']
+	message_send(admin1, channel1, "hey admin 2, apparently we can't remove each others messages")	
+	message_send(admin2, channel1, 'that sounds pretty fair to me')
+	with pytest.raises(AccessError):
+		message_remove(admin1, 2)
+	with pytest.raises(AccessError):
+		message_remove(admin2, 1)
+
+def test_message_remove_5():
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	userDict1 = auth_register('steven2@gmail.com','2hello123','2Steven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	admin1 = adminDict1['token']
+	user1 = userDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(user1, channel1, "hey admin, did you hear you can't remove other people messages")	
+	message_send(admin1, channel1, 'ridiculous, let me show you my admin rights')
+	with pytest.raises(AccessError):
+		message_remove(admin1, 1)
+	message_remove(admin1, 2)
+
+def test_message_remove_6():
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	admin1 = adminDict1['token']
+	channel1 = channelDict1['channel_id']
+	with pytest.raises(ValueError):
+		message_remove(admin1, 1)

1ef746fd4e3c1c7ce9ceaf1e17541b63cc0f2e35
Steven Lay <steven.lay@student.unsw.edu.au>
2019-10-03 22:09:23 +1000

    Added more test and formatted

--- server/message_send.py
+++ server/message_send.py
@@ -1,41 +1,82 @@
 import pytest
 
-def auth_register(email, password, name_first, name_last):
-	dummyDict = {u_id:1,token:123456}
-	return dummyDict
-
-def channels_create(token, name, is_public):
-	dummyDict = {channel_id:1}
-	return dummyDict
-
 def message_send(token, channel_id, message):
-	if len(message) > 1000:
-		raise ValueError("Message is more than 1000 characters")
 	pass
 
 	#possible errors:
-	#1.message to large
-
+	#Value error when:message to large
 	#To test message_send, we need user token from auth register
 	#auth_register(email,password,name_first,name_last) returns dict containing u_id, token
 	#and a channel_id from channels_create
 	#channels_create(token,name,is_public) returns dict containing id,name
+
+	#All test assume that nothing (users/channels/reacts/messages) exist prior to testing
 def test_message_send_1():
 	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
-	channelDict1 = channels_create(user1, 'chat1',True)
-	channelDict2 = channels_create(user1, 'chat2',True)
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
 	user1 = userDict1['token']
 	channel1 = channelDict1['channel_id']
-	#testing string > 1000
-	with pytest.raises(ValueError):
-		message_send(user1, channel1, 1001*'a')	#not legal
+	message_send(user1, channel1, 'hello')	
 
 def test_message_send_2():
 	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
-	channelDict1 = channels_create(user1, 'chat1',True)
-	channelDict2 = channels_create(user1, 'chat2',True)
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
 	user1 = userDict1['token']
 	channel1 = channelDict1['channel_id']
-	#testing string > 1001
-	assert
-		message_send(user1, channel1, 1001*'a')	#not legal
+	message_send(user1, channel1, 'HElL014%@#!9xCSzsafqw9-#*')	
+
+def test_message_send_3():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
+	user1 = userDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(user1, channel1, '1001')	
+
+def test_message_send_4():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
+	user1 = userDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(user1, channel2, '')
+
+def test_message_send_5():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
+	user1 = userDict1['token']
+	channel1 = channelDict1['channel_id']
+	#assuming '999characterstring' is taken literally
+	message_send(user1, channel2, '999characterstring')	#not legal
+
+def test_message_send_6():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
+	user1 = userDict1['token']
+	channel1 = channelDict1['channel_id']
+	#assuming '1000characterstring' is taken literally
+	message_send(user1, channel1, '1000characterstring')
+
+def test_message_send_7():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
+	user1 = userDict1['token']
+	channel1 = channelDict1['channel_id']
+	#assuming '1001characterstring' is taken literally
+	with pytest.raises(ValueError):
+		message_send(user1, channel1, '1001characterstring')
+
+def test_message_send_8():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	channelDict1 = channels_create(user1,'chat1',True)
+	channelDict2 = channels_create(user1,'chat2',True)
+	user1 = userDict1['token']
+	channel1 = channelDict1['channel_id']
+	#assuming '5000characterstring' is taken literally
+	with pytest.raises(ValueError):
+		message_send(user1, channel1, '5000characterstring')

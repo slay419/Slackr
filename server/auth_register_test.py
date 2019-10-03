0722dddbc4acd364024b29cf56627ec57779de79
Yasin Khan <z5265047@ad.unsw.edu.au>
2019-10-03 15:33:52 +1000

    added more extensive testing to register func

--- server/auth_register_test.py
+++ server/auth_register_test.py
@@ -12,37 +12,46 @@ def test_auth_register_2():
     auth_register('unusedValidemail@gmail.com', 'validpass', 'Yasin Kevin', 'Peter Steven')
 
 def test_auth_register_3():
-    with pytest.raises(ValueError):
-        auth_register('validemail2@gamil.com', 'four', 'First', 'Last')
+    auth_register('unusedValidemail@gmail.com', 'validpass', 'Same', 'Same')
 
 def test_auth_register_4():
-    with pytest.raises(ValueError):
-        auth_register('validemail2@gamil.com', 'validPasss', 'Greaterthanfiftycharactersfirstnamewhichiswaytoolong', 'Last')
+    auth_register('unusedValidemail@gmail.com', 'validpass', 'Y()@#@!#*##@', '#@$)(#)$(()#$()(#)@$)')
 
 def test_auth_register_5():
-    with pytest.raises(ValueError):
-        auth_register('validemail2@gamil.com', 'validPasss', 'First', 'GreaterthanfiftycharactersLastnamewhichiswaytoolong')
+    auth_register('unusedValidemail@gmail.com', 'validpass', 'thisisindeeddefinitelyfiftycharacterslongexactlyxx', 'thisisindeeddefinitelyfiftycharacterslongexactlyxy')
 
 def test_auth_register_6():
     with pytest.raises(ValueError):
-        auth_register('already_used_email@gamil.com', 'validPasss', 'First', 'Last')
+        auth_register('validemail2@gamil.com', 'four', 'First', 'Last')
 
 def test_auth_register_7():
     with pytest.raises(ValueError):
-        auth_register('1nVal1d3mail', 'validPasss', 'First', 'Last')
+        auth_register('validemail2@gamil.com', 'validPasss', 'Greaterthanfiftycharactersfirstnamewhichiswaytoolong', 'Last')
 
 def test_auth_register_8():
     with pytest.raises(ValueError):
-        auth_register('1nVal1d3mail', 'four', 'First', 'Last')
+        auth_register('validemail2@gamil.com', 'validPasss', 'First', 'GreaterthanfiftycharactersLastnamewhichiswaytoolong')
 
 def test_auth_register_9():
     with pytest.raises(ValueError):
-        auth_register('1nVal1d3mail', 'four', 'Greaterthanfiftycharactersfirstnamewhichiswaytoolong', 'Last')
+        auth_register('already_used_email@gamil.com', 'validPasss', 'First', 'Last')
 
 def test_auth_register_10():
     with pytest.raises(ValueError):
-        auth_register('1nVal1d3mail', 'four', 'Greaterthanfiftycharactersfirstnamewhichiswaytoolong', 'GreaterthanfiftycharactersLastnamewhichiswaytoolong')
+        auth_register('1nVal1d3mail', 'validPasss', 'First', 'Last')
 
 def test_auth_register_11():
+    with pytest.raises(ValueError):
+        auth_register('1nVal1d3mail', 'four', 'First', 'Last')
+
+def test_auth_register_12():
+    with pytest.raises(ValueError):
+        auth_register('1nVal1d3mail', 'four', 'Greaterthanfiftycharactersfirstnamewhichiswaytoolong', 'Last')
+
+def test_auth_register_13():
+    with pytest.raises(ValueError):
+        auth_register('1nVal1d3mail', 'four', 'Greaterthanfiftycharactersfirstnamewhichiswaytoolong', 'GreaterthanfiftycharactersLastnamewhichiswaytoolong')
+
+def test_auth_register_14():
     with pytest.raises(ValueError):
         auth_register('', '', '', '')

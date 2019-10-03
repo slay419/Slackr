f89915f374d6626b3b69e191f540c7172f2a081c
Steven Lay <steven.lay@student.unsw.edu.au>
2019-10-03 22:08:02 +1000

    Added more test and formatted

--- server/message_pin_test.py
+++ server/message_pin_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 def message_pin(token, message_id):
 	pass
 
@@ -9,22 +11,51 @@ def message_pin(token, message_id):
 
 #For tests 1-3, it is assumed that the user/admin is a member of the channel
 #the message is in.
-def message_pin_test_1():
-	#assuming message_id '1' doesn't contain a message
-	with raises.pytest(ValueError):
+def test_message_pin_1():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	user1 = userDict1['token']
+	admin1 = adminDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(admin1, channel1, 'testing 123')	
+	message_pin(admin1, 1)
+
+def test_message_pin_2():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	user1 = userDict1['token']
+	admin1 = adminDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(user1, channel1, 'could an admin pin this message, it is very important')	
+	message_pin(admin1, 1)
+
+def test_message_pin_3():
+	userDict1 = auth_register('steven@gmail.com','hello123','Steven','Lay')
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	user1 = userDict1['token']
+	admin1 = adminDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(admin1, channel1, 'could a user pin this message for test purposes')	
+	with raises.pytest(ValueError)
+		message_pin(user1, 1)
+
+def test_message_pin_4():
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	admin1 = adminDict1['token']
+	channel1 = channelDict1['channel_id']
+	message_send(admin1, channel1, 'I wonder what happens if I pin my own message twice')
+	message_pin(admin1, 1)
+	with raises.pytest(ValueError)
 		message_pin(admin1, 1)
 
-def message_pin_test_2():
-	#assuming user1 isn't an admin
-	with raises.pytest(ValueError):
-		message_pin(user1, 2)
-
-def message_pin_test_3():
-	message_pin(admin1, 2)
-	with raises.pytest(ValueError):
-		message_pin(admin1, 2)
-
-def message_pin_test_4():
-	#assuming message 2 is in a channel that admin1 isn't a member of
-	with raises.pytest(AccessError):
-		message_pin(admin1,2)
+def test_message_pin_5():
+	adminDict1 = auth_register('adminsteven@gmail.com','adminhello123','adminSteven','Lay')
+	channelDict1 = channels_create(user1, 'chat1',True)
+	admin1 = adminDict1['token']
+	channel1 = channelDict1['channel_id']
+	with raises.pytest(ValueError)
+		message_pin(admin1, 1)

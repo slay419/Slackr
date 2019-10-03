1f9753878b8c13fb47d9128c42afe38e6b9207e1
Kevin <kevin.chau5@outlook.com>
2019-10-03 21:22:04 +1000

    undo that last push xd

--- server/channels_create_test.py
+++ server/channels_create_test.py
@@ -25,7 +25,6 @@ token = userDict1['token']
 userDict2 = auth_register("person2@gmail.com", "password", "person", "two")
 differentToken = userDict2['token']
 
-
 # END SETUP
 
 
@@ -35,29 +34,21 @@ differentToken = userDict2['token']
 
 # Testing channel name that is exactly 21 characters long and is public
 def test_channels_create_1():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     with pytest.raises(ValueError):
         channels_create(token, "nameOfChannelIs21Long", True)
 
 # Testing channel name that is exactly 21 characters long and isprivate
 def test_channels_create_2():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     with pytest.raises(ValueError):
         channels_create(token, "nameOfChannelIs21Long", False)
 
 # Testing channel name that is significantly longer than 20 and public
 def test_channels_create_3():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     with pytest.raises(ValueError):
         channels_create(token, "nameOfChannelThatIsDefinitelyTooLong", True)
 
 # Testing channel name that is significantly longer than 20 and private
 def test_channels_create_4():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     with pytest.raises(ValueError):
         channels_create(token, "nameOfChannelThatIsDefinitelyTooLong", False)
 
@@ -68,14 +59,10 @@ NEED TO DOUBLE CHECK THIS PART BECAUSE THEY DO NOT RAISE ANY ERRORS
 '''
 # Testing channel name that is exactly 20 characters long and Public
 def test_channels_create_5():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     channels_create(token, "nameOfChannelIs20xxx", True)
 
 # Testing channel name that is exactly 20 characters long and Private
 def test_channels_create_6():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     channels_create(token, "nameOfChannelIs20xxx", False)
 
 '''
@@ -83,67 +70,43 @@ Testing channel names that are random symbols/numbers - not just characters
 '''
 # Testing numbers count as a character
 def test_channels_create_7():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     channels_create(token, "123456789", True)
 
 # Testing too many numbers will raise an error
 def test_channels_create_8():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     with pytest.raises(ValueError):
         channels_create(token, "1234567890123456789012345", True)
 
 #T Testing symbols will count as a character
 def test_channels_create_9():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     channels_create(token, "~!@#$%^&*()_-+=", False)
 
 def test_channels_create_10():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     channels_create(token, "[]{}\|;:',./<>?", False)
 
 # Testing space bar counts as a character
 def test_channels_create_11():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     channels_create(tokenm "        ", True)
 
 # Testing too many symbols will create an error
 def test_channels_create_12():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     with pytest.raises(ValueError):
         channels_create(token, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", False)
 
 # Testing a mix of letters and symbols
 def test_channels_create_13():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     channels_create(token, "name123!@#", True)
 
 # Testing a long string of letters and symbols
 def test_channels_create_14():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
     with pytest.raises(ValueError):
         channels_create(token, "VeryLongName123456!@#$%^@!#", True)
 
 # Testing expected previous behaviour with a different token
 def test_channels_create_15():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
-    userDict2 = auth_register("person2@gmail.com", "password", "person", "two")
-    differentToken = userDict2['token']
     channels_create(differentToken, "Name123!@#", False)
 
 # Testing long name with a different token
 def test_channels_create_16():
-    userDict1 = auth_register("person1@gmail.com", "password", "person", "one")
-    token = userDict1['token']
-    userDict2 = auth_register("person2@gmail.com", "password", "person", "two")
-    differentToken = userDict2['token']
     with pytest.raises(ValueError):
         channels_create(differentToken, "VeryLongName123456!@#$^$", False)

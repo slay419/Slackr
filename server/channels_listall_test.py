b0860582987b79c189e96b35c634d76fcc57fa65
Kevin <kevin.chau5@outlook.com>
2019-10-05 14:17:07 +1000

    added a few more tests and fixed some syntax

--- server/channels_listall_test.py
+++ server/channels_listall_test.py
@@ -13,12 +13,19 @@ def channels_listall(token):
 ######################## GLOBAL VARIABLES SETUP ######################
 
 ownerDict = auth_register("owner@gmail.com", "password", "owner", "privileges")
+ownerLogin = auth_login("owner@gmail.com", "password")
 owner_token = ownderDict['token']
-owner_id = ownerDict['id']
+owner_id = ownerDict['u_id']
+
+ownerDict2 = auth_register("owner2@gmail.com", "password", "owner2", "privileges")
+ownerLogin2 = auth_login("owner2@gmail.com", "password")
+owner_token2 = ownderDict['token']
+owner_id2 = ownerDict['u_id']
 
 userDict = auth_register("person1@gmail.com", "password", "person", "one")
+auth_login("person1@gmail.com", "password")
 u_token = userDict1['token']
-u_id = userDict1['id']
+u_id = userDict1['u_id']
 
 ##########################    END SETUP   ########################
 
@@ -36,7 +43,7 @@ def test_channels_listall_2():
     channel_id1 = channels_create(owner_token, "Name1", True)
     channel_id2 = channels_create(owner_token, "Name2", True)
     assert(channels_listall(u_token) == [{'id': channel_id1, 'name': "Name1"},
-                                       {'id': channel_id2, 'name': "Name2"}])
+                                         {'id': channel_id2, 'name': "Name2"}])
 
 # List of dictionaries with 3 channels created assuming order of channel_id
 def test_channels_listall_3():
@@ -44,5 +51,30 @@ def test_channels_listall_3():
     channel_id2 = channels_create(owner_token, "Name2", True)
     channel_id3 = channels_create(owner_token, "Name3", True)
     assert(channels_listall(u_token) == [{'id': channel_id1, 'name': "Name1"},
-                                       {'id': channel_id2, 'name': "Name2"},
-                                       {'id': channel_id3, 'name': "Name3"}])
+                                         {'id': channel_id2, 'name': "Name2"},
+                                         {'id': channel_id3, 'name': "Name3"}])
+
+# Testing the owner of the channel should also have the same list_all output as
+# a member
+def test_channels_listall_4():
+    channel_id1 = channels_create(owner_token, "Name1", True)
+    channel_id2 = channels_create(owner_token, "Name2", True)
+    channel_id3 = channels_create(owner_token, "Name3", True)
+    assert(channels_listall(owner_token) == [{'id': channel_id1, 'name': "Name1"},
+                                             {'id': channel_id2, 'name': "Name2"},
+                                             {'id': channel_id3, 'name': "Name3"}])
+
+
+# Testing function lists all the channels created by different owners and are
+# the same for different users
+def test_channels_listall_5():
+    channel_id1 = channels_create(owner_token, "Name1", True)
+    channel_id2 = channels_create(owner_token, "Name2", True)
+    channel_id3 = channels_create(owner_token2, "Name3", True)
+    channel_id4 = channels_create(owner_token2, "Name4", True)
+    assert(channels_listall(owner_token) == [{'id': channel_id1, 'name': "Name1"},
+                                             {'id': channel_id2, 'name': "Name2"},
+                                             {'id': channel_id3, 'name': "Name3"}
+                                             {'id': channel_id4, 'name': "Name4"}])
+    assert(channels_listall(owner_token) == channels_listall(owner_token2))
+    assert(channels_listall(owner_token) == channels_listall(u_token))

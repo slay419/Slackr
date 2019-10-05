c841d96adba7e3f198e62ba05484f23811d60ac8
Peter Chen <peter.chen1@student.unsw.edu.au>
2019-10-05 18:19:58 +1000

    Fixed format, added more tests

--- server/search.py
+++ server/search.py
@@ -2,6 +2,15 @@ import pytest
 from auth_login_test import auth_login
 import requests
 
+'''
+####################### ASSUMPTIONS ######################
+Assume entering an empty search	message is not possible as there is 
+no way to prevent this at the moment
+Assume searching will be 'find-in-word'. i.e searching for 'a' will result in 'example'
+because the letter a is present
+Assume the search message is not case sensetive because it is not possible at the moment
+'''
+
 ## This function does:
 ## Given a query string, return a collection of messages that match the query
 
@@ -12,10 +21,26 @@ def search(token, query_str):
 	messages['message1', 'message2', 'message3']
 	
     return messages
-  
-def test_search():
-	userDictionary = auth_login'johnsmith123@gmail.com', 'password123')
-	intendedUser = userDictionary['token']
+
+######################## GLOBAL VARIABLES SETUP ######################
+
+userDict = auth_login("person1@gmail.com", "password")
+u_token = userDict1['token']
+
+##########################    END SETUP   ########################
+ 
+#Test a basic search
+def test_search_1():
+	assert (search(u_token,'message') ==  ['message1', 'message2', 'message3'])
+
+#Test find-in-word search
+def test_search_2():
+	assert (search(u_token,'a') ==  ['message1', 'message2', 'message3'])
+
+#Test find-in-word search
+def test_search_3():
+	assert (search(u_token,'1') ==  ['message1', 'apple2', 'pizza1'])
 	
-	searchStr = 'searchMessage'		
-	assert (search(intendedUser,searchStr) ==  ['message1', 'message2', 'message3'])
+#Test case sensetivity
+def test_search_3():
+	assert (search(u_token,'message') ==  ['message1', 'Message2', 'mEsSaGe3'])

f49508f5625d787e9c00b2f873e8fa987f0e4cd3
Peter Chen <peter.chen1@student.unsw.edu.au>
2019-10-05 22:33:07 +1000

    Changed 'name' to 'handle'

--- server/user_profile_sethandle.py
+++ server/user_profile_sethandle.py
@@ -18,7 +18,7 @@ Assume the function returns a value error when 0 characters are entered
 ## Function will fail if:
 ## 1. Handle > 20 characters
 
-def user_profile_setname(token, handle_str):
+def user_profile_sethandle(token, handle_str):
 	if len(handle_str) > 20:
 		raise ValueError("Handle is larger than 20 characters")
 	if len(handle_str) < 1:
@@ -33,33 +33,33 @@ u_token = userDict1['token']
 ##########################    END SETUP   ########################
 
 #Test a handle larger than 20 characters
-def test_user_profile_setname_1():
+def test_user_profile_sethandle_1():
 	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 20*'JohnnySmith')
+		user_profile_sethandle(u_token, 20*'JohnnySmith')
 		
 #Test a handle with 21 characters
-def test_user_profile_setname_2():
+def test_user_profile_sethandle_2():
 	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 21*'A')
+		user_profile_sethandle(u_token, 21*'A')
 
 #Test a handle with 20 characters
-def test_user_profile_setname_3():
-	user_profile_setname(u_token, 20*'A')		
+def test_user_profile_sethandle_3():
+	user_profile_sethandle(u_token, 20*'A')		
 	
 #Test a handle with 19 characters
-def test_user_profile_setname_4():
-	user_profile_setname(u_token, 19*'A')
+def test_user_profile_sethandle_4():
+	user_profile_sethandle(u_token, 19*'A')
 	
 #Test a handle with too many special characters
-def test_user_profile_setname_5():
+def test_user_profile_sethandle_5():
 	with pytest.raises(ValueError):
-		user_profile_setname(u_token, '!@#$%^&*()+_-=|/\{}[];:?><~""')
+		user_profile_sethandle(u_token, '!@#$%^&*()+_-=|/\{}[];:?><~""')
 
 #Test a handle with 20 special characters
-def test_user_profile_setname_6():
-	user_profile_setname(u_token, '!@#$%^&*()+_-=|/\{}?')
+def test_user_profile_sethandle_6():
+	user_profile_sethandle(u_token, '!@#$%^&*()+_-=|/\{}?')
 
 #Test a handle with 0 characters
-def test_user_profile_setname_7():
+def test_user_profile_sethandle_7():
 	with pytest.raises(ValueError):
-		user_profile_setname(u_token, '')
+		user_profile_sethandle(u_token, '')

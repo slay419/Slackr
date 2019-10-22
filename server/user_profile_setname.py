e04cf3403afe26f5e4601654acf0d339f413bd16
Peter Chen <z5255813@ad.unsw.edu.au>
2019-10-22 16:22:15 +1100

    Completion of setemail, sethandle, setname, 4/8 funcs complete, not tested

--- server/user_profile_setname.py
+++ server/user_profile_setname.py
@@ -1,108 +1,22 @@
 import pytest
 from auth_login_test import auth_login 
+from flask import Flask, request
 
-## This function does:
-## Update the authorised user's first and last name
+from .data import *
 
-## Function will fail if:
-## 1. First name > 50 characters
-## 2. Last name > 50 characters
-
-'''
-####################### ASSUMPTIONS #####################
-Assume special characters should be valid
-Assume numbers should be valid
-Assume the user is authorised to call the function
-Assume the function returns a value error when 0 characters are entered
-'''
-
-
+@APP.route('user/profile/setname', methods=['PUT'])
 def user_profile_setname(token, name_first, name_last):
+
 	if len(name_first) > 50:
-		raise ValueError("First name is larger than 50 characters")
+		raise ValueError('First name is larger than 50 character')
 	if len(name_first) < 1:
-		raise ValueError("First name is less than 1 character")	
+		raise ValueError('First name is less than 1 character')	
 	if len(name_last) > 50:
-		raise ValueError("Last name is larger than 50 characters")
+		raise ValueError('Last name is larger than 50 characters')
 	if len(name_last) < 1:
-		raise ValueError("Last name is less than 1 character")		
+		raise ValueError('Last name is less than 1 character')		
+	
+	data['name_first'] = name_first
+	data['name_last'] = name_last
+
 	pass
-
-
-######################## GLOBAL VARIABLES SETUP ######################
-
-userDict = auth_login("person1@gmail.com", "password")
-u_token = userDict1['token']
-
-
-##########################    END SETUP   ########################
-
-# Test that first name is greater than 50 characters
-def test_user_profile_setname_1():
-	
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 51*'A', 'Smith')
-
-# Test that last name is greater than 50 characters
-def test_user_profile_setname_2():
-	
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 'John', 51*'A')
-		
-# Test that first name is exactly 50 characters
-def test_user_profile_setname_3():
-	
-	user_profile_setname(u_token, 50*'A', 'Chen')
-
-# Test that last name is exactly 50 characters
-def test_user_profile_setname_4():
-	
-	user_profile_setname(u_token, 'Peter', 50*'A')
-	
-# Test that first name is exactly 49 characters
-def test_user_profile_setname_5():
-	
-	user_profile_setname(u_token, 49*'A', 'Smith')
-
-# Test that last name is exactly 49 characters
-def test_user_profile_setname_6():
-	
-	user_profile_setname(u_token, 'Hayden', 49*'A')
-
-# Test that first name that has special characters
-def test_user_profile_setname_7():
-	
-	user_profile_setname(u_token, '!@#$%^&*()+_-=|/\}{][;:?><~""', 'Smith')
-
-# Test that last name that has special characters
-def test_user_profile_setname_8():
-	
-	user_profile_setname(u_token, 'John', '!@#$%^&*()+_-=|/\}{][;:?><~""')
-
-# Test a mix of special characters and letters
-def test_user_profile_setname_9():
-	
-	user_profile_setname(u_token, 'John!@#$%^&*()', 'Smith')
-
-# Test a mix of special characters and letters
-def test_user_profile_setname_10():
-	
-	user_profile_setname(u_token, 'John', 'Smith!@#$%^&*()')
-		
-# Test a valid name
-def test_user_profile_setname_11():
-	
-	user_profile_setname(u_token, 'John', 'Smith')
-
-# Test when the first name is empty
-def test_user_profile_setname_12():
-	
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, '', 'Smith')
-		
-# Test when the last name is empty
-def test_user_profile_setname_13():
-	
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 'John', '')
-		

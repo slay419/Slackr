b44c6e9f6dc7633b3bf3eb7fcdcbd310470a0125
Steven Lay <steven.lay@student.unsw.edu.au>
2019-10-22 16:46:22 +1100

    Update server.py

--- server.py
+++ server.py
@@ -1,106 +1,21 @@
-<<<<<<< HEAD
-from json import dumps
-from flask import Flask, request
-from datetime import datetime
-=======
 """Flask server"""
 import sys
 from flask_cors import CORS
 from json import dumps
 from flask import Flask, request
->>>>>>> master
 import hashlib
 import jwt
 import re
 import copy
 import time
 
-<<<<<<< HEAD
-
-APP = Flask(__name__)
-
-#GLOBAL VARIABLES
-regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-SECRET = "daenerys"
-data = {
-    'users' : [], # should have a dictionary for each user
-    'channels' : [] #shoudl have a dictionary for each channel
-
-    # e.g. {email, password, name_first, name_last, u_id, permission_id, handle, token, profile, is_logged}
-
-    #e.g {'channel_id' : 1234 , 'name' : channelname, 'owners' : [u_id1, u_id2...], members : [u_id, u_id2....], 'ispublic': True }
-}
-
-#check if email is valid
-def valid_email(email):
-    if(re.search(regex,email)):
-        return True
-    else:
-        return False
-
-#abstraction for returning global data
-def get_data():
-    global data
-    return data
-
-#abstraction for returning json string
-def send_sucess(data):
-    return dumps(data)
-
-def send_error(message):
-    return dumps({
-        '_error': message
-    })
-
-#encodes token given string and SECRET
-def generate_token(string):
-    global SECRET
-    return jwt.encode({'string' : string, 'time' : time.time()}, SECRET, algorithm='HS256').decode('utf-8')
-
-#decodes token given string and SECRET
-def decode_token(token):
-    global SECRET
-    decoded = jwt.decode(token.encode('utf-8'), SECRET, algorithms=['HS256'])
-    return decoded['string']
-
-#generates hash for string
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def is_logged_in(token):
-
-    u_id = decode_token(token)
-    user = user_dict(u_id)
-    if user == None:
-        return False
-    if token in user['tokens']:
-        return True
-    else:
-        return False
-
-def user_dict(u_id):
-    data = get_data()
-    for user in data['users']:
-        if u_id == user['u_id']:
-            return user
-    return None
-
-def channel_dict(channel_id):
-    data = get_data()
-    for channel in data['channels']:
-        if channel_id == channel['channel_id']:
-            return channel
-    return None
-
-=======
 from backend.functions.data import *
 from backend.functions.channel_functions import *
 
 APP = Flask(__name__)
 
->>>>>>> master
 #########################   AUTH FUNCTIONS  ###########################
-
+##
 #REGISTER
 @APP.route('/auth/register', methods = ['POST'])
 def create():
@@ -154,11 +69,7 @@ def create():
         'tokens'  : [],
         'profile' : None
     })
-<<<<<<< HEAD
-    return send_sucess({
-=======
     return send_success({
->>>>>>> master
         'u_id': u_id,
         'token' : token
     })
@@ -180,11 +91,7 @@ def connect():
             u_id = user['u_id']
             token = generate_token(u_id)
             user['tokens'].append(token)
-<<<<<<< HEAD
-            return send_sucess({
-=======
             return send_success({
->>>>>>> master
                 'u_id' : u_id,
                 'token': token
             })
@@ -213,11 +120,7 @@ def invite():
         if u_id == user:
             return send_error('user already part of channel')
     channel['members'].append(u_id)
-<<<<<<< HEAD
-    return send_sucess({})
-=======
     return send_success({})
->>>>>>> master
 
 
 
@@ -242,11 +145,7 @@ def join():
     else:
         return send_error('user does not have rightts')
 
-<<<<<<< HEAD
-    return send_sucess({})
-=======
     return send_success({})
->>>>>>> master
 
 @APP.route('/auth/logout', methods = ['PUT'])
 def logout():
@@ -256,137 +155,16 @@ def logout():
     user = user_dict(u_id)
     user['tokens'].remove(token)
 
-<<<<<<< HEAD
-    return send_sucess({})
-=======
     return send_success({})
->>>>>>> master
 
 #########################   CHANNEL FUNCTIONS  ###########################
 
 @APP.route('/channels/create', methods = ['POST'])
-<<<<<<< HEAD
-def channels_create():
-=======
 def channel_create():
->>>>>>> master
     token = request.form.get('token')
     name = request.form.get('name')
     is_public = request.form.get('is_public')
     if not is_logged_in(token):
-<<<<<<< HEAD
-        send_error("User not logged in")
-    if len(name) > 20:
-        return send_error("Name of channel is longer than 20 characters.")
-    data = get_data()
-    owner_id = decode_token(token)
-    print(owner_id)
-    get_user_name(owner_id)
-    print(f"owner's name is: {get_user_name(owner_id)}")
-
-    # Give the channel an ID which corresponds to the number created e.g. 1st channel is ID1 ...
-    new_channel_id = len(data['channels']) + 1
-    # Create a dictionary with all the relevant info and append to data
-    dict = {
-        'channel_id': new_channel_id,
-        'name': name,
-        'owners': [owner_id],
-        'members': [],
-        'is_public': is_public,
-        'messages': []
-    }
-    data['channels'].append(dict)
-    print(data['channels'])
-    return send_sucess( {
-        'channel_id': new_channel_id
-    })
-
-
-def get_user_name(u_id):
-    data = get_data()
-    for user in data['users']:
-        if u_id == user['u_id']:
-            return {user['name_first'], user['name_last']}
-    return None
-
-
-@APP.route('/channel/messages', methods = ['GET'])
-#input: token, channelid, start
-#user must be a member of the channel
-def showmessages():
-    data = get_data()
-    token = request.args.get('token')
-    channel_id = int(request.args.get('channel_id')) #must be valid channel
-    start = int(request.args.get('start')) #cannot be >= no. of messages in channel
-    end = start + 50
-    messages = []
-    #Checking channel_id is valid
-    newchannel = {}
-    for channel in data['channels']:
-        if(channel['channel_id'] == channel_id):
-            newchannel.update(channel)
-    if not newchannel:
-        return send_error('Invalid channel_id')
-    #Checking length of messages
-    if(start > len(newchannel['messages'])):
-        return send_error('Start index is > then amount of messages')
-    u_id = decode_token(token)
-    #Checking if user is authorised in correct channel
-    if(u_id not in newchannel['members'] and u_id not in newchannel['owners']):
-        return send_error('user is not in correct channel')
-    index = start
-    for i in range(index,len(newchannel['messages']) + 1):
-        messages.append(newchannel['messages'][i])
-    if end > len(newchannel['messages']):
-        end = -1
-    return dumps({'messages': messages, 'start': start, 'end': end,}, indent=4, sort_keys=True, default=str)
-
-    #given start return end which is start + 50 or -1 if theres no more messages
-            
-#########################   MESSAGE FUNCTIONS  ###########################
-#check valid channel aswell
-@APP.route('/message/send', methods = ['POST'])
-#input: token,channelid,message
-def send():
-    #Initialising all data from input
-    data = get_data()
-    token = request.form.get('token')
-    channel_id = int(request.form.get('channel_id'))
-    message = request.form.get('message')
-    #The message_id will be 1 + length of messages in a specific channel
-    newchannel = {}
-    for channel in data['channels']:
-        if(channel['channel_id'] == channel_id):
-            newchannel.update(channel)
-        else:
-            print(channel_id)
-            print(channel['channel_id'])
-    message_id = 1 + len(newchannel['messages'])
-    if(len(message) > 1000):
-        return send_error('message is more than 1000 characters')
-    #Obtaining u_id from token
-    u_id = decode_token(token)
-    #if user hasn't joined the channel they are sending a message in):
-    if(u_id not in newchannel['members'] and u_id not in newchannel['owners']):
-        return send_error('user is not in correct channel')
-    message_dict = {
-        'message_id': message_id,
-        'u_id': u_id,
-        'message': message,
-        'time_created': datetime.now(),
-        'is_unread': True,
-        'reacts': [],
-        'is_pinned': False,
-    }
-    for channel in data['channels']:
-        if(channel['channel_id'] == channel_id):
-            channel['messages'].insert(0, message_dict)
-            print(newchannel['messages'])
-    return send_sucess({'message_id': message_id})
-    
-if __name__ == "__main__":
-    APP.run(debug=True)
-=======
         return send_error(f"User: {decode_token(token)} not logged in")
     return send_success(channels_create(token, name, is_public))
 
@@ -457,4 +235,3 @@ def removeowner():
 
 if __name__ == "__main__":
     APP.run(port=(sys.argv[1] if len(sys.argv) > 1 else 5000))
->>>>>>> master

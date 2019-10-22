399abaa737aee6267ad964cb6649077b8635571a
Steven Lay <steven.lay@student.unsw.edu.au>
2019-10-22 17:39:24 +1100

    Added functions

--- backend/functions/message_functions.py
+++ backend/functions/message_functions.py
@@ -1,11 +1,10 @@
-@APP.route('/channel/messages', methods = ['GET'])
+from .data import *
+from datetime import datetime
+
 #input: token, channelid, start
 #user must be a member of the channel
-def showmessages():
+def channel_messages(token, channel_id, start):
     data = get_data()
-    token = request.args.get('token')
-    channel_id = int(request.args.get('channel_id')) #must be valid channel
-    start = int(request.args.get('start')) #cannot be >= no. of messages in channel
     end = start + 50
     messages = []
     #Checking channel_id is valid
@@ -23,32 +22,29 @@ def showmessages():
     if(u_id not in newchannel['members'] and u_id not in newchannel['owners']):
         return send_error('user is not in correct channel')
     index = start
-    for i in range(index,len(newchannel['messages']) + 1):
+    print("Index: " + str(index))
+    print("Length of messages: " + str(len(newchannel['messages'])+1))
+    print(newchannel['messages'])
+    for i in range(index,len(newchannel['messages'])):
         messages.append(newchannel['messages'][i])
     if end > len(newchannel['messages']):
         end = -1
-    return dumps({'messages': messages, 'start': start, 'end': end,}, indent=4, sort_keys=True, default=str)
+    return {'messages': messages, 'start': start, 'end': end,}
 
     #given start return end which is start + 50 or -1 if theres no more messages
             
 #########################   MESSAGE FUNCTIONS  ###########################
 #check valid channel aswell
-@APP.route('/message/send', methods = ['POST'])
+
 #input: token,channelid,message
-def send():
+def message_send(token, channel_id, message):
     #Initialising all data from input
     data = get_data()
-    token = request.form.get('token')
-    channel_id = int(request.form.get('channel_id'))
-    message = request.form.get('message')
     #The message_id will be 1 + length of messages in a specific channel
     newchannel = {}
     for channel in data['channels']:
         if(channel['channel_id'] == channel_id):
             newchannel.update(channel)
-        else:
-            print(channel_id)
-            print(channel['channel_id'])
     message_id = 1 + len(newchannel['messages'])
     if(len(message) > 1000):
         return send_error('message is more than 1000 characters')
@@ -69,5 +65,4 @@ def send():
     for channel in data['channels']:
         if(channel['channel_id'] == channel_id):
             channel['messages'].insert(0, message_dict)
-            print(newchannel['messages'])
-    return send_sucess({'message_id': message_id})
+    return {'message_id': message_id}

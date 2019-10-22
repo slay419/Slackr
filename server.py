fe0e549759c919c6d461100508e2230097bde050
Steven Lay <steven.lay@student.unsw.edu.au>
2019-10-22 16:21:49 +1100

    added message functions

--- server.py
+++ server.py
@@ -256,9 +256,9 @@ def channels_create():
     }
     data['channels'].append(dict)
     print(data['channels'])
-    return {
+    return send_sucess( {
         'channel_id': new_channel_id
-    }
+    })
 
 
 def get_user_name(u_id):
@@ -268,43 +268,65 @@ def get_user_name(u_id):
             return {user['name_first'], user['name_last']}
     return None
 
-'''
+
 @APP.route('/channel/messages', methods = ['GET'])
 #input: token, channelid, start
 #user must be a member of the channel
 def showmessages():
     data = get_data()
     token = request.args.get('token')
-    channel_id = request.args.get('channel_id') #must be valid channel
-    start = request.args.get('start') #cannot be >= no. of messages in channel
-    end = send + 50
-    #Checking length of messages
+    channel_id = int(request.args.get('channel_id')) #must be valid channel
+    start = int(request.args.get('start')) #cannot be >= no. of messages in channel
+    end = start + 50
+    messages = []
+    #Checking channel_id is valid
+    newchannel = {}
     for channel in data['channels']:
         if(channel['channel_id'] == channel_id):
-'''
+            newchannel.update(channel)
+    if not newchannel:
+        return send_error('Invalid channel_id')
+    #Checking length of messages
+    if(start > len(newchannel['messages'])):
+        return send_error('Start index is > then amount of messages')
+    u_id = decode_token(token)
+    #Checking if user is authorised in correct channel
+    if(u_id not in newchannel['members'] and u_id not in newchannel['owners']):
+        return send_error('user is not in correct channel')
+    index = start
+    for i in range(index,len(newchannel['messages']) + 1):
+        messages.append(newchannel['messages'][i])
+    if end > len(newchannel['messages']):
+        end = -1
+    return dumps({'messages': messages, 'start': start, 'end': end,}, indent=4, sort_keys=True, default=str)
+
+    #given start return end which is start + 50 or -1 if theres no more messages
             
 #########################   MESSAGE FUNCTIONS  ###########################
+#check valid channel aswell
 @APP.route('/message/send', methods = ['POST'])
 #input: token,channelid,message
 def send():
     #Initialising all data from input
     data = get_data()
     token = request.form.get('token')
-    channel_id = request.form.get('channel_id')
+    channel_id = int(request.form.get('channel_id'))
     message = request.form.get('message')
     #The message_id will be 1 + length of messages in a specific channel
+    newchannel = {}
     for channel in data['channels']:
         if(channel['channel_id'] == channel_id):
-            newchannel = channel
-    message_id = 1 + len([newchannel['messages']])
-    if(len(message > 1000)):
+            newchannel.update(channel)
+        else:
+            print(channel_id)
+            print(channel['channel_id'])
+    message_id = 1 + len(newchannel['messages'])
+    if(len(message) > 1000):
         return send_error('message is more than 1000 characters')
     #Obtaining u_id from token
-    for user in data['users']:
-        if(user['token'] == token):
-            u_id = user['u_id']
-    #if(user hasn't joined the channel they are sending a message in):
-    if(u_id not in newchannel['members'] or u_id not in newchannel['owners']):
+    u_id = decode_token(token)
+    #if user hasn't joined the channel they are sending a message in):
+    if(u_id not in newchannel['members'] and u_id not in newchannel['owners']):
         return send_error('user is not in correct channel')
     message_dict = {
         'message_id': message_id,
@@ -316,10 +338,10 @@ def send():
         'is_pinned': False,
     }
     for channel in data['channels']:
-        if(channel == channel_id):
+        if(channel['channel_id'] == channel_id):
             channel['messages'].insert(0, message_dict)
-    return send_success({'message_id': message_id})
+            print(newchannel['messages'])
+    return send_sucess({'message_id': message_id})
     
 if __name__ == "__main__":
     APP.run(debug=True)
-

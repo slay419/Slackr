a7d745f6e9f3c1cc15332235ab4039b92a0f9743
Peter Chen <z5255813@ad.unsw.edu.au>
2019-10-24 20:34:46 +1100

    Optimised with helper funcs

--- backend/functions/profile_functions.py
+++ backend/functions/profile_functions.py
@@ -2,17 +2,19 @@ from .data import *
 
 def user_profile_setemail(token, email):
 	data = get_data()
-	myID = decode_token(token)
-	
+	u_id = decode_token(token)
+	userData = user_dict(u_id)
+
+	if userData == None:
+		raise ValueError("User ID does not exist")
 	if valid_email(email) is False:
 		raise ValueError("Email entered is not a valid email")
 	if is_email_free(email) == 0:
 		raise ValueError("Email is already in use")
 	# At this point, the email is valid and untaken
-	for user in data['users']:
-		if myID == data['users']['u_id']:
-			data['users']['email'] = email
-			print("Successfully updated user's email")
+	
+	userData['email'] = email
+	print("Successfully updated user's email")
 
 	return {}
 
@@ -29,23 +31,31 @@ def is_email_free(email):
 
 def user_profile_sethandle(token, handle_str):
 	data = get_data()
+	u_id = decode_token(token)
+	userData = user_dict(u_id)
+
+	if userData == None:
+		raise ValueError("User ID does not exist")
 	if len(handle_str) > 20:
 		raise ValueError("Handle is larger than 20 characters")
 	if len(handle_str) < 1:
 		raise ValueError("Handle is less than 1 character")	
 
 	myID = decode_token(token)
-	for user in data['users']:
-		if myID == data['users']['u_id']:
-			data['users']['handle'] = handle_str
-			print("Successfully updated user's handle")
+	
+	userData['handle'] = handle_str
+	print("Successfully updated user's handle")
 
 	return {}
 
 def user_profile_setname(token, name_first, name_last):
 
 	data = get_data()
+	u_id = decode_token(token)
+	userData = user_dict(u_id)
 
+	if userData == None:
+		raise ValueError("User ID does not exist")
 	if len(name_first) > 50:
 		raise ValueError('First name is larger than 50 character')
 	if len(name_first) < 1:
@@ -55,12 +65,9 @@ def user_profile_setname(token, name_first, name_last):
 	if len(name_last) < 1:
 		raise ValueError('Last name is less than 1 character')	
 
-	myID = decode_token(token)
-	for user in data['users']:
-		if myID == data['users']['u_id']:
-			data['users']['name_first'] = name_first
-			data['users']['name_last'] = name_last
-			print("Successfully updated user's first and last name")
+	userData['name_first'] = name_first
+	userData['name_last'] = name_last
+	print("Successfully updated user's first and last name")
 
 	return {}
 
@@ -68,18 +75,12 @@ def user_profile(token,u_id):
 
 	data = get_data()
 
-	valid = 0
+	userData = user_dict(u_id)
 
-	for user in data['users']:
-		if u_id == data['users']['u_id']:
-			valid = 1
-
-	if valid == 0:
+	if userData == None:
 		raise ValueError("User ID does not exist")
 		return
 	else:
-		for user in data['users']:
-			if data['users']['u_id'] == u_id:
-				new_dict = {'email':data['users']['email'], 'name_first':data['users']['firstName'], 'name_last':data['users']['lastName'], 'handle_str':user["handle"]}
+		new_dict = {'email':userData['email'], 'name_first':userData['firstName'], 'name_last':userData['lastName'], 'handle_str':userData["handle"]}
 	print("Successfully found and located user's information")
 	return new_dict	#returns dict containing {email,name_first,name_last,handle_str}

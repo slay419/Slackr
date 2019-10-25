7aca8b970fd44d8ad59e7ad8a34e5bf363af6388
Steven Lay <steven.lay@student.unsw.edu.au>
2019-10-25 23:31:27 +1100

    added more error checks

--- backend/functions/message_functions.py
+++ backend/functions/message_functions.py
@@ -38,29 +38,28 @@ def message_sendlater(token, channel_id, message, time_sent):
         if(channel['channel_id'] == channel_id):
             channel['messages'].insert(0, message_dict)
             data['messages'].append(message_dict)
-            print('return 3')
     return {'message_id': message_id}
     
 #input: token,channelid,message
 def message_send(token, channel_id, message):
     #Initialising all data from input
     data = get_data()
-    
+
     #The message_id will be 1 + length of messages in a specific channel
     newchannel = {}
     for channel in data['channels']:
         if(channel['channel_id'] == channel_id):
             newchannel.update(channel)
     message_id = 1 + len(data['messages'])
+    if not is_valid_channel(channel_id):
+        raise ValueError('Channel ID is not a valid channel')
     if(len(message) > 1000):
-        print('return 1')
-        return send('message is more than 1000 characters')
+        raise ValueError('Message is more than 1000 characters')
     #Obtaining u_id from token
     u_id = decode_token(token)
     #if user hasn't joined the channel they are sending a message in):
     if(is_member(u_id, channel_id) == False):
-        print('return 2')
-        return send('user is not in correct channel')
+        raise AccessError('Authorised user has not joined the channel they are trying to post to')
     dt = datetime.now()
     timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
     message_dict = {
@@ -77,15 +76,15 @@ def message_send(token, channel_id, message):
             channel['messages'].insert(0, message_dict)
             data['messages'].append(message_dict)
 
-    print(message_dict)
     return {'message_id': message_id}
 
 def message_remove(token, message_id):
     data = get_data()
     #remove message from global dict
+    if not is_valid_message(message_id):
+        return ValueError('Message (based on ID) no longer exists')
     for message in data['messages']:
         if message['message_id'] == message_id:
-            print("we found a match")
             data['messages'].remove(message)
     #Searching for correct messagedict in the channels list
     channels = data['channels']
@@ -94,7 +93,6 @@ def message_remove(token, message_id):
         for messagedict in messagelist:
             if messagedict['message_id'] == message_id:
                 messagelist.remove(messagedict) 
-    print (data['messages'])
     return {}
     
 def message_edit(token, message_id, message):

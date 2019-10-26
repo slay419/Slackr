8324e04f2a646d1a0204c61c0275d39783213743
Yasin Khan <z5265047@ad.unsw.edu.au>
2019-10-26 16:18:19 +1100

    channel details test

--- backend/channel_details_test.py
+++ backend/channel_details_test.py
@@ -1,58 +1,104 @@
 import pytest
-from auth_register_test import auth_register
-from channels_create_test import channels_create
-from channel_invite_test import channel_invite
-from error import AccessError
-
-def channel_details(token, channel_id):
-    pass
-
+from functions.auth_functions import auth_register, auth_logout
+from functions.channel_functions import channels_create, channel_invite, channel_details
+from functions.data import *
 
 #SETUP BEGIN
-dict1 = auth_register('validemail@gmail.com', 'correctpass')
-token1 = dict1['token']
-
-dict2 = auth_register('validemail2@gmail.com', 'correctpass2')
-token2 = dict2['token']
-u_id2 = dict2['u_id']
-
-dict3 = channels_create(token1, 'somechannel1', 1)
-channel_id1 = dict3['channel_id']
-
-dict4 = channels_create(token2, 'somechannel2', 0)
-channel_id2 = dict4['channel_id']
 
 #SETUP END
 
-#user1 views details of channel he created
+#user views details of channel he created
 def test_channel_details_1():
-    channel_details(token1, channel_id1)
+    reset_data()
 
-#user2 views details of channel he created
+    dict1 = auth_register('email1@gmail.com', 'validpass', 'Yasin', 'Kevin')
+    token1 = dict1['token']
+    u_id1 = dict1['u_id']
+
+    dict2 = channels_create(token1, 'someChannel', 1)
+    channel_id1 = dict2['channel_id']
+
+    assert(channel_details(token1, channel_id1) == {
+        'name' : 'someChannel',
+        'owner_members' : [{'u_id' : u_id1, 'name_first' : 'Yasin' , 'name_last' : 'Kevin'}],
+        'all_members' :  [{'u_id' : u_id1, 'name_first' : 'Yasin' , 'name_last' : 'Kevin'}]
+    })
+
+#user invites someone then views details of channel he created
 def test_channel_details_2():
-    channel_details(token2, channel_id2)
+    reset_data()
 
-#user1 tries to view details of channel with id that does not exist
+    dict1 = auth_register('email1@gmail.com', 'validpass', 'Yasin', 'Kevin')
+    token1 = dict1['token']
+    u_id1 = dict1['u_id']
+
+    dict2 = auth_register('email2@gmail.com', 'validpass', 'Peter', 'Steven')
+    u_id2 = dict2['u_id']
+
+    dict3 = channels_create(token1, 'someChannel', 1)
+    channel_id1 = dict3['channel_id']
+
+    channel_invite(token1, channel_id1, u_id2)
+
+    assert(channel_details(token1, channel_id1) == {
+        'name' : 'someChannel',
+        'owner_members' : [{'u_id' : u_id1, 'name_first' : 'Yasin' , 'name_last' : 'Kevin'}],
+        'all_members' :  [{'u_id' : u_id1, 'name_first' : 'Yasin' , 'name_last' : 'Kevin'}, {'u_id' : u_id2, 'name_first' : 'Peter' , 'name_last' : 'Steven'}]
+    })
+
+#user invites someone then invitee views channel just joined
 def test_channel_details_3():
-    with pytest.raises(ValueError):
-        channel_details(token1, channel_id1 + channel_id2)
+    reset_data()
 
-#user2 tries to view details of channel with id that does not exist
+    dict1 = auth_register('email1@gmail.com', 'validpass', 'Yasin', 'Kevin')
+    token1 = dict1['token']
+    u_id1 = dict1['u_id']
+
+    dict2 = auth_register('email2@gmail.com', 'validpass', 'Peter', 'Steven')
+    u_id2 = dict2['u_id']
+    token2 = dict2['token']
+
+    dict3 = channels_create(token1, 'someChannel', 1)
+    channel_id1 = dict3['channel_id']
+
+    channel_invite(token1, channel_id1, u_id2)
+
+    assert(channel_details(token2, channel_id1) == {
+        'name' : 'someChannel',
+        'owner_members' : [{'u_id' : u_id1, 'name_first' : 'Yasin' , 'name_last' : 'Kevin'}],
+        'all_members' :  [{'u_id' : u_id1, 'name_first' : 'Yasin' , 'name_last' : 'Kevin'}, {'u_id' : u_id2, 'name_first' : 'Peter' , 'name_last' : 'Steven'}]
+    })
+
+
+#user tries to view details of channel with id that does not exist
 def test_channel_details_4():
-    with pytest.raises(ValueError):
-        channel_details(token2, channel_id1 + channel_id2)
+    reset_data()
 
-#user2 tries to view details of channel he is not part of
+    dict1 = auth_register('email1@gmail.com', 'validpass', 'Yasin', 'Kevin')
+    token1 = dict1['token']
+    u_id1 = dict1['u_id']
+
+    dict2 = channels_create(token1, 'someChannel', 1)
+    channel_id1 = dict2['channel_id']
+
+    with pytest.raises(ValueError):
+        channel_details(token1, channel_id1 + 1)
+
+
+#user tries to view details of channel he is not part of
 def test_channel_details_5():
+    reset_data()
+
+    dict1 = auth_register('email1@gmail.com', 'validpass', 'Yasin', 'Kevin')
+    token1 = dict1['token']
+    u_id1 = dict1['u_id']
+
+    dict2 = auth_register('email2@gmail.com', 'validpass', 'Peter', 'Steven')
+    u_id2 = dict2['u_id']
+    token2 = dict2['token']
+
+    dict3 = channels_create(token1, 'someChannel', 1)
+    channel_id1 = dict3['channel_id']
+
     with pytest.raises(AccessError):
         channel_details(token2, channel_id1)
-
-#user1 tries to view details of a channel he is not part of
-def test_channel_details_6():
-    with pytest.raises(AccessError):
-        channel_details(token1, channel_id2)
-
-#invite user2 to join channel and then view it.   
-def test_channel_details_7():
-    channel_invite(token2, channel_id1, u_id2)
-    channel_details(token2, channel_id1)

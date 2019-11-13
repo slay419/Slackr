7fdb9b58342170308375a8e41253d1c9bd7f5bed
Kevin <kevin.chau5@outlook.com>
2019-11-13 22:46:46 +1100

    started fixing search

--- backend/search_test.py
+++ backend/search_test.py
@@ -17,13 +17,9 @@ Assume searching will be 'find-in-word'. i.e searching for 'a' will result in 'e
 because the letter a is present
 Assume the search message is not case sensetive because it is not possible at the moment
 '''
-def floor_message_time(message_id):
-	message = message_dict(message_id)
-	message['time_created'] = math.floor(message['time_created'])
 
-#Testing for exact match
-def test_search_1():
-	######################## BEGIN SETUP ######################
+######################## BEGIN SETUP ######################
+def setup():
 	reset_data()
 	ownerDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
 	owner_token = ownerDict['token']
@@ -71,9 +67,18 @@ def test_search_1():
 	floor_message_time(message_id5)
 	now = datetime.now()
 	m5_time = math.floor(now.replace().timestamp())
-	##########################    END SETUP   ########################
 
-	assert (search(owner_token,"message12") ==  {
+	return owner_token, owner_id, u_token, u_id, channel, second_channel
+##########################    END SETUP   ########################
+
+def floor_message_time(message_id):
+	message = message_dict(message_id)
+	message['time_created'] = math.floor(message['time_created'])
+
+#Testing for exact match
+def test_search_1():
+	owner_token, owner_id, u_token, u_id, channel, second_channel = setup()
+	assert (search(owner_token, "message12") ==  {
 		'messages': [
 			{
 				'message_id': message_id2,
@@ -89,56 +94,7 @@ def test_search_1():
 
 #Test find-in-word search and also across other channels
 def test_search_2():
-	######################## BEGIN SETUP ######################
-	reset_data()
-	ownerDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	owner_token = ownerDict['token']
-	owner_id = ownerDict['u_id']
-
-	# Second user
-	userDict = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-
-	# Create channel for messages to send to
-	channel = channels_create(owner_token, "channel name", True)
-	channel_id = channel['channel_id']
-
-	# Create channel for messages to send to
-	second_channel = channels_create(u_token, "second channel", True)
-	channel_id2 = second_channel['channel_id']
-
-	message1 = message_send(owner_token, channel_id, "message1")
-	message_id1 = message1['message_id']
-	floor_message_time(message_id1)
-	now = datetime.now()
-	m1_time = math.floor(now.replace().timestamp())
-
-	message2 = message_send(owner_token, channel_id, "message12")
-	message_id2 = message2['message_id']
-	floor_message_time(message_id2)
-	now = datetime.now()
-	m2_time = math.floor(now.replace().timestamp())
-
-	message3 = message_send(owner_token, channel_id, "message3")
-	message_id3 = message3['message_id']
-	floor_message_time(message_id3)
-	now = datetime.now()
-	m3_time = math.floor(now.replace().timestamp())
-
-	message4 = message_send(owner_token, channel_id, "message4")
-	message_id4 = message4['message_id']
-	floor_message_time(message_id4)
-	now = datetime.now()
-	m4_time = math.floor(now.replace().timestamp())
-
-	message5 = message_send(u_token, channel_id2, "message32")
-	message_id5 = message5['message_id']
-	floor_message_time(message_id5)
-	now = datetime.now()
-	m5_time = math.floor(now.replace().timestamp())
-	##########################    END SETUP   ########################
-
+	owner_token, owner_id, u_token, u_id, channel, second_channel = setup()
 	assert (search(owner_token, "message") ==  {
 		'messages': [
 			{
@@ -191,56 +147,7 @@ def test_search_2():
 
 #Test find-in-word search
 def test_search_3():
-	######################## BEGIN SETUP ######################
-	reset_data()
-	ownerDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	owner_token = ownerDict['token']
-	owner_id = ownerDict['u_id']
-
-	# Second user
-	userDict = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-
-	# Create channel for messages to send to
-	channel = channels_create(owner_token, "channel name", True)
-	channel_id = channel['channel_id']
-
-	# Create channel for messages to send to
-	second_channel = channels_create(u_token, "second channel", True)
-	channel_id2 = second_channel['channel_id']
-
-	message1 = message_send(owner_token, channel_id, "message1")
-	message_id1 = message1['message_id']
-	floor_message_time(message_id1)
-	now = datetime.now()
-	m1_time = math.floor(now.replace().timestamp())
-
-	message2 = message_send(owner_token, channel_id, "message12")
-	message_id2 = message2['message_id']
-	floor_message_time(message_id2)
-	now = datetime.now()
-	m2_time = math.floor(now.replace().timestamp())
-
-	message3 = message_send(owner_token, channel_id, "message3")
-	message_id3 = message3['message_id']
-	floor_message_time(message_id3)
-	now = datetime.now()
-	m3_time = math.floor(now.replace().timestamp())
-
-	message4 = message_send(owner_token, channel_id, "message4")
-	message_id4 = message4['message_id']
-	floor_message_time(message_id4)
-	now = datetime.now()
-	m4_time = math.floor(now.replace().timestamp())
-
-	message5 = message_send(u_token, channel_id2, "message32")
-	message_id5 = message5['message_id']
-	floor_message_time(message_id5)
-	now = datetime.now()
-	m5_time = math.floor(now.replace().timestamp())
-	##########################    END SETUP   ########################
-
+	owner_token, owner_id, u_token, u_id, channel, second_channel = setup()
 	assert (search(owner_token, "message1") ==  {
 		'messages': [
 			{
@@ -266,112 +173,14 @@ def test_search_3():
 
 #Test no match
 def test_search_4():
-	######################## BEGIN SETUP ######################
-	reset_data()
-	ownerDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	owner_token = ownerDict['token']
-	owner_id = ownerDict['u_id']
-
-	# Second user
-	userDict = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-
-	# Create channel for messages to send to
-	channel = channels_create(owner_token, "channel name", True)
-	channel_id = channel['channel_id']
-
-	# Create channel for messages to send to
-	second_channel = channels_create(u_token, "second channel", True)
-	channel_id2 = second_channel['channel_id']
-
-	message1 = message_send(owner_token, channel_id, "message1")
-	message_id1 = message1['message_id']
-	floor_message_time(message_id1)
-	now = datetime.now()
-	m1_time = math.floor(now.replace().timestamp())
-
-	message2 = message_send(owner_token, channel_id, "message12")
-	message_id2 = message2['message_id']
-	floor_message_time(message_id2)
-	now = datetime.now()
-	m2_time = math.floor(now.replace().timestamp())
-
-	message3 = message_send(owner_token, channel_id, "message3")
-	message_id3 = message3['message_id']
-	floor_message_time(message_id3)
-	now = datetime.now()
-	m3_time = math.floor(now.replace().timestamp())
-
-	message4 = message_send(owner_token, channel_id, "message4")
-	message_id4 = message4['message_id']
-	floor_message_time(message_id4)
-	now = datetime.now()
-	m4_time = math.floor(now.replace().timestamp())
-
-	message5 = message_send(u_token, channel_id2, "message32")
-	message_id5 = message5['message_id']
-	floor_message_time(message_id5)
-	now = datetime.now()
-	m5_time = math.floor(now.replace().timestamp())
-	##########################    END SETUP   ########################
-
+	owner_token, owner_id, u_token, u_id, channel, second_channel = setup()
 	assert (search(owner_token,"no match") ==  {
 		'messages': []
 	})
 
 # Testing for empty string returns everything
 def test_search_5():
-	######################## BEGIN SETUP ######################
-	reset_data()
-	ownerDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	owner_token = ownerDict['token']
-	owner_id = ownerDict['u_id']
-
-	# Second user
-	userDict = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-
-	# Create channel for messages to send to
-	channel = channels_create(owner_token, "channel name", True)
-	channel_id = channel['channel_id']
-
-	# Create channel for messages to send to
-	second_channel = channels_create(u_token, "second channel", True)
-	channel_id2 = second_channel['channel_id']
-
-	message1 = message_send(owner_token, channel_id, "message1")
-	message_id1 = message1['message_id']
-	floor_message_time(message_id1)
-	now = datetime.now()
-	m1_time = math.floor(now.replace().timestamp())
-
-	message2 = message_send(owner_token, channel_id, "message12")
-	message_id2 = message2['message_id']
-	floor_message_time(message_id2)
-	now = datetime.now()
-	m2_time = math.floor(now.replace().timestamp())
-
-	message3 = message_send(owner_token, channel_id, "message3")
-	message_id3 = message3['message_id']
-	floor_message_time(message_id3)
-	now = datetime.now()
-	m3_time = math.floor(now.replace().timestamp())
-
-	message4 = message_send(owner_token, channel_id, "message4")
-	message_id4 = message4['message_id']
-	floor_message_time(message_id4)
-	now = datetime.now()
-	m4_time = math.floor(now.replace().timestamp())
-
-	message5 = message_send(u_token, channel_id2, "message32")
-	message_id5 = message5['message_id']
-	floor_message_time(message_id5)
-	now = datetime.now()
-	m5_time = math.floor(now.replace().timestamp())
-	##########################    END SETUP   ########################
-
+	owner_token, owner_id, u_token, u_id, channel, second_channel = setup()
 	assert (search(owner_token,"") ==  {
 		'messages': [
 			{

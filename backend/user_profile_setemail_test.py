e5d8b6630cbec3795da3a350adf73fbeeb6d17a8
Steven Lay <steven.lay@student.unsw.edu.au>
2019-11-17 14:21:15 +1100

    Fixed more stuff

--- backend/user_profile_setemail_test.py
+++ backend/user_profile_setemail_test.py
@@ -1,16 +1,14 @@
-from functions.auth_functions import auth_register
-from functions.channel_functions import channels_create, channel_join, channels_list
-from functions.misc_functions import admin_userpermission_change
-from functions.profile_functions import user_profile, user_profile_sethandle, user_profile_setemail
-
-from functions.data import *
-
-from functions.exceptions import ValueError, AccessError
-
 #pylint: disable=missing-docstring
 #pylint: disable=unused-variable
-
 import pytest
+
+from functions.auth_functions import auth_register
+from functions.profile_functions import user_profile, user_profile_setemail
+
+from functions.data import reset_users
+
+from functions.exceptions import ValueError
+
 
 ###SIDE NOTE: Web resources were used in order to create the basic algorithm to determine
 ### what a valid email is
@@ -25,28 +23,28 @@ Assume the function returns a ValueError if the string is empty
 
 # Testing an invalid email
 def test_user_profile_setemail_1():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	with pytest.raises(ValueError):
-		user_profile_setemail(u_token, 'myemail.com')
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    with pytest.raises(ValueError):
+        user_profile_setemail(u_token, 'myemail.com')
 
 # Testing an empty email
 def test_user_profile_setemail_2():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	with pytest.raises(ValueError):
-		user_profile_setemail(u_token, '')
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    with pytest.raises(ValueError):
+        user_profile_setemail(u_token, '')
 
 # Testing an long but valid email
 def test_user_profile_setemail_3():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setemail(u_token, 'abcdefghijklmnopqrstuvwxyz@email.com')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setemail(u_token, 'abcdefghijklmnopqrstuvwxyz@email.com')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'abcdefghijklmnopqrstuvwxyz@email.com',
         'name_first' : 'firstname',
         'name_last' : 'lastname',
@@ -55,12 +53,12 @@ def test_user_profile_setemail_3():
 
 # Testing an short but valid email
 def test_user_profile_setemail_4():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setemail(u_token, 'abc@email.com')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setemail(u_token, 'abc@email.com')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'abc@email.com',
         'name_first' : 'firstname',
         'name_last' : 'lastname',
@@ -69,9 +67,9 @@ def test_user_profile_setemail_4():
 
 # Testing a email that is being used
 def test_user_profile_setemail_5():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "person", "one")
-	userDict2 = auth_register("person2@gmail.com", "password", "person2", "two")
-	u_token2 = userDict2['token']
-	with pytest.raises(ValueError):
-		user_profile_setemail(u_token2, 'person1@gmail.com')
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "person", "one")
+    user_dict2 = auth_register("person2@gmail.com", "password", "person2", "two")
+    u_token2 = user_dict2['token']
+    with pytest.raises(ValueError):
+        user_profile_setemail(u_token2, 'person1@gmail.com')
--- backend/user_profile_setname_test.py
+++ backend/user_profile_setname_test.py
@@ -1,16 +1,14 @@
 #pylint: disable=missing-docstring
 #pylint: disable=unused-variable
-
-from functions.auth_functions import auth_register
-from functions.channel_functions import channels_create, channel_join, channels_list
-from functions.misc_functions import admin_userpermission_change
-from functions.profile_functions import user_profile, user_profile_sethandle, user_profile_setemail, user_profile_setname
-
-from functions.data import *
-
-from functions.exceptions import ValueError, AccessError
-
+#pylint: disable=anomalous-backslash-in-string
 import pytest
+from functions.auth_functions import auth_register
+from functions.profile_functions import user_profile, user_profile_setname
+
+from functions.data import reset_users
+
+from functions.exceptions import ValueError
+
 
 ## This function does:
 ## Update the authorised user's first and last name
@@ -30,29 +28,28 @@ Assume the function returns a value error when 0 characters are entered
 
 # Test that first name is greater than 50 characters
 def test_user_profile_setname_1():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 51*'A', 'Smith')
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    with pytest.raises(ValueError):
+        user_profile_setname(u_token, 51*'A', 'Smith')
 
 # Test that last name is greater than 50 characters
 def test_user_profile_setname_2():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 'John', 51*'A')
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    with pytest.raises(ValueError):
+        user_profile_setname(u_token, 'John', 51*'A')
 
 # Test that first name is exactly 50 characters
 def test_user_profile_setname_3():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 50*'A', 'Chen')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 50*'A', 'Chen')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 50*'A',
         'name_last' : 'Chen',
@@ -61,12 +58,12 @@ def test_user_profile_setname_3():
 
 # Test that last name is exactly 50 characters
 def test_user_profile_setname_4():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 'Peter', 50*'A')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 'Peter', 50*'A')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 'Peter',
         'name_last' : 50*'A',
@@ -75,12 +72,12 @@ def test_user_profile_setname_4():
 
 # Test that first name is exactly 49 characters
 def test_user_profile_setname_5():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 49*'A', 'Smith')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 49*'A', 'Smith')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 49*'A',
         'name_last' : 'Smith',
@@ -89,12 +86,12 @@ def test_user_profile_setname_5():
 
 # Test that last name is exactly 49 characters
 def test_user_profile_setname_6():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 'Hayden', 49*'A')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 'Hayden', 49*'A')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 'Hayden',
         'name_last' : 49*'A',
@@ -103,12 +100,12 @@ def test_user_profile_setname_6():
 
 # Test that first name that has special characters
 def test_user_profile_setname_7():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, '!@#$%^&*()+_-=|/\}{][;:?><~""', 'Smith')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, '!@#$%^&*()+_-=|/\}{][;:?><~""', 'Smith')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : '!@#$%^&*()+_-=|/\}{][;:?><~""',
         'name_last' : 'Smith',
@@ -117,12 +114,12 @@ def test_user_profile_setname_7():
 
 # Test that last name that has special characters
 def test_user_profile_setname_8():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 'John', '!@#$%^&*()+_-=|/\}{][;:?><~""')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 'John', '!@#$%^&*()+_-=|/\}{][;:?><~""')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 'John',
         'name_last' : '!@#$%^&*()+_-=|/\}{][;:?><~""',
@@ -131,12 +128,12 @@ def test_user_profile_setname_8():
 
 # Test a mix of special characters and letters
 def test_user_profile_setname_9():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 'John!@#$%^&*()', 'Smith')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 'John!@#$%^&*()', 'Smith')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 'John!@#$%^&*()',
         'name_last' : 'Smith',
@@ -145,12 +142,12 @@ def test_user_profile_setname_9():
 
 # Test a mix of special characters and letters
 def test_user_profile_setname_10():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 'John', 'Smith!@#$%^&*()')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 'John', 'Smith!@#$%^&*()')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 'John',
         'name_last' : 'Smith!@#$%^&*()',
@@ -159,12 +156,12 @@ def test_user_profile_setname_10():
 
 # Test a valid name
 def test_user_profile_setname_11():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	user_profile_setname(u_token, 'John', 'Smith')
-	assert (user_profile(u_token, u_id) == {
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    user_profile_setname(u_token, 'John', 'Smith')
+    assert (user_profile(u_token, u_id) == {
         'email' : 'person1@gmail.com',
         'name_first' : 'John',
         'name_last' : 'Smith',
@@ -173,18 +170,18 @@ def test_user_profile_setname_11():
 
 # Test when the first name is empty
 def test_user_profile_setname_12():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, '', 'Smith')
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    with pytest.raises(ValueError):
+        user_profile_setname(u_token, '', 'Smith')
 
 # Test when the last name is empty
 def test_user_profile_setname_13():
-	reset_users()
-	userDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	u_token = userDict['token']
-	u_id = userDict['u_id']
-	with pytest.raises(ValueError):
-		user_profile_setname(u_token, 'John', '')
+    reset_users()
+    user_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    u_token = user_dict1['token']
+    u_id = user_dict1['u_id']
+    with pytest.raises(ValueError):
+        user_profile_setname(u_token, 'John', '')

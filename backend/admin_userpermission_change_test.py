ccb91fc859d11ff2f588d9645b364b53b6c643f9
Kevin <kevin.chau5@outlook.com>
2019-11-17 14:42:55 +1100

    fixed tab issue

--- backend/admin_userpermission_change_test.py
+++ backend/admin_userpermission_change_test.py
@@ -23,80 +23,80 @@ admin's permission_id
 
 # Test if the user id is not valid
 def test_admin_userpermission_change_1():
-	reset_users()
-	admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
-	admin_token = admin_dict1['token']
-	admin_id = admin_dict1['u_id']
-	invalid_u_id = admin_id + 1
-	with pytest.raises(ValueError):
-		admin_userpermission_change(admin_token, invalid_u_id, 1)
+    reset_users()
+    admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
+    admin_token = admin_dict1['token']
+    admin_id = admin_dict1['u_id']
+    invalid_u_id = admin_id + 1
+    with pytest.raises(ValueError):
+        admin_userpermission_change(admin_token, invalid_u_id, 1)
 
 # Test if the permission is not valid
 def test_admin_userpermission_change_2():
-	reset_users()
-	admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
-	admin_token = admin_dict1['token']
-	admin_id = admin_dict1['u_id']
-	with pytest.raises(ValueError):
-		admin_userpermission_change(admin_token, admin_id, 50)
+    reset_users()
+    admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
+    admin_token = admin_dict1['token']
+    admin_id = admin_dict1['u_id']
+    with pytest.raises(ValueError):
+        admin_userpermission_change(admin_token, admin_id, 50)
 
 # Test if the permission is not valid
 def test_admin_userpermission_change_3():
-	reset_users()
-	admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
-	admin_token = admin_dict1['token']
-	admin_id = admin_dict1['u_id']
-	with pytest.raises(ValueError):
-		admin_userpermission_change(admin_token, admin_id, -30)
+    reset_users()
+    admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
+    admin_token = admin_dict1['token']
+    admin_id = admin_dict1['u_id']
+    with pytest.raises(ValueError):
+        admin_userpermission_change(admin_token, admin_id, -30)
 
 # Test if the user is a member and calls the function
 def test_admin_userpermission_change_4():
-	reset_users()
-	admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
-	admin_token = admin_dict1['token']
-	admin_id = admin_dict1['u_id']
+    reset_users()
+    admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
+    admin_token = admin_dict1['token']
+    admin_id = admin_dict1['u_id']
 
-	member_dict1 = auth_register("member@gmail.com", "password", "member", "privileges")
-	member_token = member_dict1['token']
-	member_id = member_dict1['u_id']
-	with pytest.raises(AccessError):
-		admin_userpermission_change(member_token, member_id, 3)
+    member_dict1 = auth_register("member@gmail.com", "password", "member", "privileges")
+    member_token = member_dict1['token']
+    member_id = member_dict1['u_id']
+    with pytest.raises(AccessError):
+        admin_userpermission_change(member_token, member_id, 3)
 
 # Test if the user has permission
 def test_admin_userpermission_change_5():
-	reset_users()
-	admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
-	admin_token = admin_dict1['token']
-	admin_id = admin_dict1['u_id']
-	admin_userpermission_change(admin_token, admin_id, 1)
-	assert(get_permission_id(admin_id) == 1)
+    reset_users()
+    admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
+    admin_token = admin_dict1['token']
+    admin_id = admin_dict1['u_id']
+    admin_userpermission_change(admin_token, admin_id, 1)
+    assert(get_permission_id(admin_id) == 1)
 
 # Test if the user has permission
 def test_admin_userpermission_change_6():
-	reset_users()
-	admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
-	admin_token = admin_dict1['token']
-	admin_id = admin_dict1['u_id']
-	admin_userpermission_change(admin_token, admin_id, 2)
-	assert(get_permission_id(admin_id) == 2)
+    reset_users()
+    admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
+    admin_token = admin_dict1['token']
+    admin_id = admin_dict1['u_id']
+    admin_userpermission_change(admin_token, admin_id, 2)
+    assert(get_permission_id(admin_id) == 2)
 
 
 # owner cant change admin user permsssion
 def test_admin_userpermission_change_7():
-	reset_users()
-	admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
-	admin_token = admin_dict1['token']
-	admin_id = admin_dict1['u_id']
+    reset_users()
+    admin_dict1 = auth_register("admin@gmail.com", "password", "admin", "privileges")
+    admin_token = admin_dict1['token']
+    admin_id = admin_dict1['u_id']
 
-	member_dict1 = auth_register("member@gmail.com", "password", "member", "privileges")
-	member_token = member_dict1['token']
-	member_id = member_dict1['u_id']
+    member_dict1 = auth_register("member@gmail.com", "password", "member", "privileges")
+    member_token = member_dict1['token']
+    member_id = member_dict1['u_id']
 
-	#admin changes member to owner (permission_id 3 --> 2)
-	admin_userpermission_change(admin_token, member_id, 2)
-	owner_token = member_token
-	owner_id = member_id
+    #admin changes member to owner (permission_id 3 --> 2)
+    admin_userpermission_change(admin_token, member_id, 2)
+    owner_token = member_token
+    owner_id = member_id
 
-	#owner tries to change admin's permission (LOL)
-	with pytest.raises(AccessError):
-		admin_userpermission_change(owner_token, admin_id, 2)
+    #owner tries to change admin's permission (LOL)
+    with pytest.raises(AccessError):
+        admin_userpermission_change(owner_token, admin_id, 2)

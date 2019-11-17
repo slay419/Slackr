7449024226defabf7a5e516ce09e9c1cc4ccc6a6
Kevin <kevin.chau5@outlook.com>
2019-11-17 14:20:55 +1100

    fixed pytest for listall

--- backend/user_profile_listall_test.py
+++ backend/user_profile_listall_test.py
@@ -2,12 +2,10 @@
 #pylint: disable=unused-variable
 
 from functions.auth_functions import auth_register
-from functions.misc_functions import admin_userpermission_change
-from functions.profile_functions import user_profile, user_profile_sethandle, user_profile_setemail, user_listall
+from functions.profile_functions import user_listall
 
-from functions.data import *
+from functions.data import reset_data
 
-import pytest
 
 '''
 ####################### ASSUMPTIONS #####################
@@ -30,12 +28,18 @@ def setup():
 # Test list with one user
 def test_user_listall_1():
     u_token, u_id = setup()
-    assert(user_listall(u_token) =={'users': [{'email': 'person1@gmail.com',
-        'handle_str': 'personone',
-        'name_first': 'person',
-        'name_last': 'one',
-        'profile_img_url': None,
-        'u_id': 101}]})
+    assert(user_listall(u_token) == {
+        'users': [
+            {
+                'email': 'person1@gmail.com',
+                'handle_str': 'personone',
+                'name_first': 'person',
+                'name_last': 'one',
+                'profile_img_url': None,
+                'u_id': 101
+            }
+        ]
+    })
 
 # Test list with two users
 def test_user_listall_2():
@@ -45,19 +49,26 @@ def test_user_listall_2():
     u_token2 = user_dict2['token']
     u_id2 = user_dict2['u_id']
 
-    assert(user_listall(u_token) == {'users': [{'email': 'person1@gmail.com',
-        'handle_str': 'personone',
-        'name_first': 'person',
-        'name_last': 'one',
-        'profile_img_url': None,
-        'u_id': 101},
-        {'email': 'person2@gmail.com',
-        'handle_str': 'person2two',
-        'name_first': 'person2',
-        'name_last': 'two',
-        'profile_img_url': None,
-        'u_id': 102}]}
-)
+    assert(user_listall(u_token) == {
+        'users': [
+            {
+                'email': 'person1@gmail.com',
+                'handle_str': 'personone',
+                'name_first': 'person',
+                'name_last': 'one',
+                'profile_img_url': None,
+                'u_id': 101
+            },
+            {
+                'email': 'person2@gmail.com',
+                'handle_str': 'person2two',
+                'name_first': 'person2',
+                'name_last': 'two',
+                'profile_img_url': None,
+                'u_id': 102
+            }
+        ]
+    })
 
 # Test list with three users
 def test_user_listall_3():
@@ -72,21 +83,31 @@ def test_user_listall_3():
     u_token3 = user_dict3['token']
     u_id3 = user_dict3['u_id']
 
-    assert(user_listall(u_token) == {'users': [{'email': 'person1@gmail.com',
-        'handle_str': 'personone',
-        'name_first': 'person',
-        'name_last': 'one',
-        'profile_img_url': None,
-        'u_id': 101},
-        {'email': 'person2@gmail.com',
-        'handle_str': 'person2two',
-        'name_first': 'person2',
-        'name_last': 'two',
-        'profile_img_url': None,
-        'u_id': 102},
-        {'email': 'person3@gmail.com',
-        'handle_str': 'person3three',
-        'name_first': 'person3',
-        'name_last': 'three',
-        'profile_img_url': None,
-        'u_id': 103}]})
+    assert(user_listall(u_token) == {
+        'users': [
+            {
+                'email': 'person1@gmail.com',
+                'handle_str': 'personone',
+                'name_first': 'person',
+                'name_last': 'one',
+                'profile_img_url': None,
+                'u_id': 101
+            },
+            {
+                'email': 'person2@gmail.com',
+                'handle_str': 'person2two',
+                'name_first': 'person2',
+                'name_last': 'two',
+                'profile_img_url': None,
+                'u_id': 102
+            },
+            {
+                'email': 'person3@gmail.com',
+                'handle_str': 'person3three',
+                'name_first': 'person3',
+                'name_last': 'three',
+                'profile_img_url': None,
+                'u_id': 103
+            }
+        ]
+    })

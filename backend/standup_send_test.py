534fe6a589c67eaa2cb4b95c5fd3dda514bdb5c2
Steven Lay <steven.lay@student.unsw.edu.au>
2019-11-17 14:39:41 +1100

    Fixed more

--- backend/standup_send_test.py
+++ backend/standup_send_test.py
@@ -1,13 +1,15 @@
-from functions.auth_functions import auth_register
-from functions.channel_functions import channels_create, channel_join, channels_list
-from functions.misc_functions import admin_userpermission_change, standup_send, standup_start
-from functions.profile_functions import user_profile, user_profile_sethandle, user_profile_setemail
+#pylint: disable=missing-docstring
+#pylint: disable=unused-variable
+import pytest
 
-from functions.data import *
+from functions.auth_functions import auth_register
+from functions.channel_functions import channels_create
+from functions.misc_functions import standup_send, standup_start
+
+from functions.data import reset_data
 
 from functions.exceptions import ValueError, AccessError
 
-import pytest
 
 '''
 ####################### ASSUMPTIONS ######################
@@ -23,99 +25,99 @@ Assume "time_left" can retrieve the date_time object from standup_start
 
 ######################## GLOBAL VARIABLES SETUP ######################
 def setup():
-	reset_data()
-	# First user
-	ownerDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	owner_token = ownerDict['token']
+    reset_data()
+    # First user
+    owner_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    owner_token = owner_dict1['token']
 
-	# Second user
-	userDict = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
-	u_token = userDict['token']
+    # Second user
+    user_dict1 = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
+    u_token = user_dict1['token']
 
-	return owner_token, u_token
+    return owner_token, u_token
 ##########################    END SETUP   ########################
 
 # Test for a channel that does not exist
 def test_standup_send_1():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	with pytest.raises(ValueError):
-		standup_send(owner_token, channel_id1 + 1, "Hey Lets start the standup")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    with pytest.raises(ValueError):
+        standup_send(owner_token, channel_id1 + 1, "Hey Lets start the standup")
 
 # Test for a message with 1001 characters
 def test_standup_send_2():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	with pytest.raises(ValueError):
-		standup_send(owner_token, channel_id1, 1001*"A")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    with pytest.raises(ValueError):
+        standup_send(owner_token, channel_id1, 1001*"A")
 
 # Test for a message with 1000 characters
 def test_standup_send_3():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	standup_send(owner_token, channel_id1, 1000*"A")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    standup_send(owner_token, channel_id1, 1000*"A")
 
 # Test for a message with 999 characters
 def test_standup_send_4():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	standup_send(owner_token, channel_id1, 999*"A")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    standup_send(owner_token, channel_id1, 999*"A")
 
 # Test for an empty message
 def test_standup_send_5():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	with pytest.raises(ValueError):
-		standup_send(owner_token, channel_id1, "")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    with pytest.raises(ValueError):
+        standup_send(owner_token, channel_id1, "")
 
 # Test for starting a standup on a channel the user is not part of
 def test_standup_send_6():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	with pytest.raises(AccessError):
-		standup_send(u_token, channel_id1, "Hey Lets start the standup")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    with pytest.raises(AccessError):
+        standup_send(u_token, channel_id1, "Hey Lets start the standup")
 
 # Test for starting a standup on a channel the user is not part of
 def test_standup_send_7():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	with pytest.raises(AccessError):
-		standup_send(u_token, channel_id1, "Hey Lets start the standup")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    with pytest.raises(AccessError):
+        standup_send(u_token, channel_id1, "Hey Lets start the standup")
 
 
 # Test for sending a message when the standup has ended
 def test_standup_send_8():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	with pytest.raises(ValueError):
-		standup_send(owner_token, channel_id1, "Hey Lets start the standup")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    with pytest.raises(ValueError):
+        standup_send(owner_token, channel_id1, "Hey Lets start the standup")
 
 # Test for sending a message when there is no standup
 def test_standup_send_9():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	with pytest.raises(ValueError):
-		standup_send(owner_token, channel_id1, "Hey Lets start the standup")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    with pytest.raises(ValueError):
+        standup_send(owner_token, channel_id1, "Hey Lets start the standup")
 
 # Test for a normal message
 def test_standup_send_10():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	standup_send(owner_token, channel_id1, "Hey Lets start the standup")
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    standup_send(owner_token, channel_id1, "Hey Lets start the standup")
--- backend/standup_start_test.py
+++ backend/standup_start_test.py
@@ -1,14 +1,16 @@
-from functions.auth_functions import auth_register
-from functions.channel_functions import channels_create, channel_join, channels_list
-from functions.misc_functions import admin_userpermission_change, standup_send, standup_start
-from functions.profile_functions import user_profile, user_profile_sethandle, user_profile_setemail
+#pylint: disable=missing-docstring
+#pylint: disable=unused-variable
+from datetime import datetime, timedelta
+import pytest
 
-from functions.data import *
+from functions.auth_functions import auth_register
+from functions.channel_functions import channels_create
+from functions.misc_functions import standup_start
+
+from functions.data import reset_data
 
 from functions.exceptions import ValueError, AccessError
 
-from datetime import datetime, timedelta, timezone
-import pytest
 
 '''
 ####################### ASSUMPTIONS ######################
@@ -20,56 +22,56 @@ Assume you cannot have two standups running at the same time on the same channel
 
 ######################## GLOBAL VARIABLES SETUP ######################
 def setup():
-	reset_data()
-	# First user
-	ownerDict = auth_register("person1@gmail.com", "password", "firstname", "lastname")
-	owner_token = ownerDict['token']
+    reset_data()
+    # First user
+    owner_dict1 = auth_register("person1@gmail.com", "password", "firstname", "lastname")
+    owner_token = owner_dict1['token']
 
-	# Second user
-	userDict = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
-	u_token = userDict['token']
+    # Second user
+    user_dict1 = auth_register("person2@gmail.com", "password123", "firstname", "lastname")
+    u_token = user_dict1['token']
 
-	return owner_token, u_token
+    return owner_token, u_token
 ##########################    END SETUP   ########################
 
 
 # Testing a invalid channel_id
 def test_standup_start_1():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	with pytest.raises(ValueError):
-		standup_start(owner_token, channel_id1 + 1, 10)
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    with pytest.raises(ValueError):
+        standup_start(owner_token, channel_id1 + 1, 10)
 
 # Testing a channel id that the user is not part
 def test_standup_start_2():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	with pytest.raises(AccessError):
-		standup_start(u_token, channel_id1, 10)
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    with pytest.raises(AccessError):
+        standup_start(u_token, channel_id1, 10)
 
 # Testing a channel id that the user is not part
 def test_standup_start_3():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	with pytest.raises(AccessError):
-		standup_start(u_token, channel_id1, 10)
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    with pytest.raises(AccessError):
+        standup_start(u_token, channel_id1, 10)
 
 # Testing a channel id that the user is part of
 def test_standup_start_4():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	endTime = datetime.now() + timedelta(minutes=15)
-	assert(standup_start(owner_token, channel_id1, 10))
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    endtime = datetime.now() + timedelta(minutes=15)
+    assert standup_start(owner_token, channel_id1, 10)
 
 # Testing starting a standup that is already running
 def test_standup_start_5():
-	owner_token, u_token = setup()
-	channel1 = channels_create(owner_token, 'SERVER1', True)
-	channel_id1 = channel1['channel_id']
-	standup_start(owner_token, channel_id1, 10)
-	with pytest.raises(ValueError):
-		standup_start(owner_token, channel_id1, 10)
+    owner_token, u_token = setup()
+    channel1 = channels_create(owner_token, 'SERVER1', True)
+    channel_id1 = channel1['channel_id']
+    standup_start(owner_token, channel_id1, 10)
+    with pytest.raises(ValueError):
+        standup_start(owner_token, channel_id1, 10)
